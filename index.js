637644e026f7be215a9dcf64dccfc2b4a5e77f45
Ryan Mui <reachryanmui@gmail.com>
2018-11-25 22:36:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {}
 var recipes2 = new Object();
 
-function updateObjectWithKeyAndValue(object, key, value) {
+/*function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]:value})
 }
 
@@ -27,3 +27,4 @@ function deleteFromObjectByKey(object, key) {
 deleteFromObjectByKey(recipes, 'mer')
 
 //console.log(object2)
+*/
