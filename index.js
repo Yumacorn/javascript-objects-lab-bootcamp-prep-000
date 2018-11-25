54e20e64af798d44de435cae44e47ee8e7e17603
Ryan Mui <reachryanmui@gmail.com>
2018-11-25 23:22:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 console.log(recipes + "1")
 recipes = updateObjectWithKeyAndValue(recipes, "chicken", "orange")
-console.log(recipes)
+console.log(recipes + "2")
 recipes = updateObjectWithKeyAndValue(recipes, "second chicken", "sesame")
 console.log(recipes)
 
