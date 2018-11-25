53ef9a5dd8242a6d2ec11e2305222a8d86253d6f
Ryan Mui <reachryanmui@gmail.com>
2018-11-25 23:02:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]:value})
 }
+
+console.log(recipes)
+recipes = updateObjectWithKeyAndValue(recipes, "chicken", "orange")
+console.log(recipes)
+recipes = updateObjectWithKeyAndValue(recipes, "chicken", "sesame")
+
+console.log(recipes)
 /*
-console.log(recipes)
-recipes = updateObjectWithKeyAndValue(recipes, "far", "mer")
-console.log(recipes)
-recipes = updateObjectWithKeyAndValue(recipes, "ro", "mer")
-
-console.log(recipes)
-
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
   return Object.assign(object, {[key]:value})
 }
